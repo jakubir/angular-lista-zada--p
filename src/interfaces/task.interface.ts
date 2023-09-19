8ba72c7d550d4d216e625682db18ff9740821637
jakubir <irlajakub@gmail.com>
2023-09-19 07:41:56 +0200

    1 file modified, 2 added, 1 deleted

---
+++ src/interfaces/task.interface.ts
@@ -0,0 +1,4 @@
+export interface TaskInterface {
+  id: number;
+  text: string;
+}
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
 
-import { Task } from './task';
+import { Task } from './task/task.component';
+import { TaskInterface } from './interfaces/task.interface';
 
 @Component({
   selector: 'my-app',
@@ -25,9 +26,10 @@ import { Task } from './task';
     <hr>
     <h1>Wykonane</h1>
     <ul>
-      <li *ngFor="let task of doneTasks"><task [text]="task.text" [done]="true" (statusChanged)="endTask(task.id)" /></li>
+      <li *ngFor="let task of doneTasks"><task [text]="task.text" [done]="true" (statusChanged)="restoreTask(task.id)" /></li>
     </ul>
-  `,
+    <button (click)="removeDoneTasks()">Usuń wykonane zadania</button>
+    `,
 })
 export class App {
   pendingTasks: any = [
@@ -42,14 +44,17 @@ export class App {
     this.pendingTasks.push({ id: this.number, text: 'Zadanie ' + this.number });
     this.number++;
   }
-  endTask = (id: number): void => {
+  removeDoneTasks() {
+    this.doneTasks = [];
+  }
+  endTask = (id: number) => {
     let taskToEnd = this.pendingTasks.find((task: any) => task.id == id);
-    this.pendingTasks.splice(this.pendingTasks.indexOf(taskToEnd));
+    let a = this.pendingTasks.splice(this.pendingTasks.indexOf(taskToEnd), 1);
     this.doneTasks.push({ id: id, text: 'Zadanie ' + id });
   };
-  retoreTask = (id: number): void => {
+  restoreTask = (id: number) => {
     let taskToRestore = this.doneTasks.find((task: any) => task.id == id);
-    this.doneTasks.splice(this.doneTasks.indexOf(taskToRestore));
+    this.doneTasks.splice(this.doneTasks.indexOf(taskToRestore), 1);
     this.pendingTasks.push({ id: id, text: 'Zadanie ' + id });
   };
 }
